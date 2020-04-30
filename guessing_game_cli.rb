d9b8ff868bda8b00e6ad28802632d56838a9723f
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-30 00:19:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,11 +4,11 @@ def run_guessing_game
   num = rand(6) + 1
   # print 'Type a random number and test your skills'+ "\n"
   your_guess = gets.chomp
-  your_guess = your_guess.to_i
-  equality = num == your_guess
    if your_guess == 'exit'
    return print 'Goodbye!'
   end
+  your_guess = your_guess.to_i
+  equality = num == your_guess
   if (equality)
     return print 'You guessed the correct number!'
   end
